d8339034b9552e71a887b20a9295bf594780666c
hanijeel <leejinah7368@gmail.com>
2020-11-25 09:54:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Box extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      bgColor: ""
+    };
+  }
+
+  changeColor = () => {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    this.setState({bgColor: "#" + randomColor})
+  }
+
+  render() {
+    return (
+      <div 
+        className="box" 
+        onMouseOver={() => this.changeColor()} 
+        style={{backgroundColor: this.state.bgColor}}
+      >
+      </div>
+      );
+  }
 }
 
-export default App;
+class App extends React.Component {  
+  render() {
+    const boxes = [];
+    for (let i=0; i<100; i++) {
+      boxes.push(<Box></Box>)
+    }
+
+    return(
+      <div className="board">
+        <div className="container">
+          {boxes}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+// Haven's Refactured Code
+// export default App;
+
+// import React from 'react';
+// import './App.css';
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       boxColors: Array(200),
+//     };
+//   }
+
+//   handleMouseOver = (i) => {
+//     console.log("mouse over handled")
+//     const newColors = this.state.boxColors.slice();
+//     let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+//     newColors[i] = "#" + randomColor;
+//     this.setState({ boxColors: newColors });
+//   }
+
+//   render() {
+//     const boxes = [];
+
+//     for (let i = 0; i < this.state.boxColors.length; i++) {
+//       boxes.push(this.renderBox(i));
+//     }
+//     return (
+//       <div>
+//         {boxes}
+//       </div>
+//     )
+//   }
+
+//   renderBox = (i) => {
+//     return (
+//       <Box
+//         key={i}
+//         handleMouseOver={() => this.handleMouseOver(i)}
+//         bgColor={this.state.boxColors[i]}
+//       />
+//     )
+//   }
+// }
+
+
+// function Box(props) {
+//   return (
+//     <div
+//       className="box"
+//       onMouseOver={props.handleMouseOver}
+//       style={{ backgroundColor: props.bgColor }}
+//     >
+
+//     </div>
+//   );
+// }
+
+// export default App;
